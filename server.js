072b28659d8fd8124ba6c127c8c6956fa65e40c3
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2016-12-06 13:24:32 +0100

    Fix Zomato fetching

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ var SLACK_CHANNEL = process.env.SLACK_CHANNEL;
 var jsdom = require('jsdom');
 var HipChatClient = require('hipchat-client');
 var SlackIncomingWebhooks = require('@slack/client').IncomingWebhook;
+var sys = require('sys')
+var exec = require('child_process').exec;
 
 if (HIPCHAT_API_KEY) {
     var hipchat = new HipChatClient(HIPCHAT_API_KEY);
@@ -136,23 +138,41 @@ var fetchMenu = function(cb){
     );
 };
 
-var fetchMenuZomato = function(zomato_id, cb){
-    var CANTEEN_WEBPAGE = 'https://www.zomato.com/bratislava/' + zomato_id + '/daily-menu';
-    console.log(CANTEEN_WEBPAGE);
+var zomatoFetch = function(zomato_id, cb) {
+  var CANTEEN_WEBPAGE = 'https://www.zomato.com/bratislava/' + zomato_id + '/daily-menu';
+  console.log(CANTEEN_WEBPAGE);
 
-    jsdom.env(
-        CANTEEN_WEBPAGE,
+  var child;
+  var cmd = "curl '" + CANTEEN_WEBPAGE + "' -H 'accept-language: sk-SK,sk;q=0.8,cs;q=0.6,en-US;q=0.4,en;q=0.2' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' -H 'cookie: zl=en' --compressed";
+
+  // executes `pwd`
+  child = exec(cmd, function (error, stdout, stderr) {
+    // sys.print('stdout: ' + stdout);
+    // sys.print('stderr: ' + stderr);
+    cb(error, stdout);
+  });
+};
+
+var fetchMenuZomato = function(zomato_id, cb){
+  zomatoFetch(zomato_id, function(err, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      jsdom.env(
+        body,
         ['http://code.jquery.com/jquery.js'],
         function (err, window) {
-            if (err) {
-                console.log(err);
-                return cb(err)
-            }
-            var text = window.$('.tmi-group:first').text();
-            console.log(text);
-            return cb(err, text);
+          if (err) {
+            console.log(err);
+            return cb(err)
+          }
+          var text = window.$('.tmi-group:first').text();
+          console.log(text);
+          return cb(err, text);
         }
-    );
+      );
+    }
+  });
 };
 
 var breakfastNotification = require('cron').CronJob;
